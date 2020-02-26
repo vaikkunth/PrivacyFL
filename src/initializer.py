13562cc7025feef7a67cf8f96cb1fb080bfb2b7b
vaikkunth <31374772+vaikkunth@users.noreply.github.com>
2020-02-26 14:46:51 -0500

    Add files via upload

---
+++ src/initializer.py
@@ -0,0 +1,120 @@
+import config
+import datetime
+import numpy as np
+import pickle
+import random
+
+from client_agent import ClientAgent
+from server_agent import ServerAgent
+from directory import Directory
+from pyspark.sql import SparkSession
+from sklearn.datasets import load_digits
+
+from utils import data_formatting
+
+from utils.print_config import print_config
+from utils.model_evaluator import ModelEvaluator
+from sklearn.preprocessing import MinMaxScaler
+
+
+class Initializer:
+    def __init__(self, num_clients, num_servers, iterations):
+        """
+        Offline stage of simulation. Initializes clients and servers for iteration as well as gives each client its data.
+        :param num_clients: number of clients to be use for simulation
+        :param num_servers: number of servers to be use for simulation. Personalized coding required if greater than 1.
+        :param iterations: number of iterations to run simulation for
+        """
+
+        global len_per_iteration
+        if config.USING_PYSPARK:
+            spark = SparkSession.builder.appName('SecureFederatedLearning').getOrCreate()  # initialize spark session
+            spark.sparkContext.setLogLevel("ERROR")  # supress sparks messages
+
+        digits = load_digits()  # using sklearn's MNIST dataset
+        X, y = digits.data, digits.target
+
+        scaler = MinMaxScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
+
+        X_train, X_test = X[:-config.LEN_TEST], X[-config.LEN_TEST:]
+        y_train, y_test = y[:-config.LEN_TEST], y[-config.LEN_TEST:]
+
+        # extract only amount that we require
+        number_of_samples = 0
+        for client_name in config.client_names:
+            len_per_iteration = config.LENS_PER_ITERATION[client_name]
+            number_of_samples += len_per_iteration * iterations
+
+        X_train = X_train[:number_of_samples]
+        y_train = y_train[:number_of_samples]
+
+        # df = data_formatting.create_spark_df(X_train, y_train)
+        # train_datasets = df.randomSplit([1/num_clients] * num_clients)  # split up dataset for clients
+
+        # client_to_datasets = {}  # do the splitting by iterations now since clients cannot split the datasets dynamically very easily
+        # for i, dataset in enumerate(train_datasets):
+        #    client_datasets = dataset.randomSplit([1/iterations] * iterations) # split evenly into iterations
+        #    client_to_datasets[i] = client_datasets
+
+        client_to_datasets = data_formatting.partition_data(X_train, y_train, config.client_names, iterations,
+                                                            config.LENS_PER_ITERATION, cumulative=config.USING_CUMULATIVE, pyspark=config.USING_PYSPARK)
+
+        #print_config(len_per_iteration=config.LEN_PER_ITERATION)
+        print('\n \n \nSTARTING SIMULATION \n \n \n')
+
+        active_clients = {'client_agent' + str(i) for i in range(num_clients)}
+        self.clients = {
+            'client_agent' + str(i): ClientAgent(agent_number=i,
+                                                 train_datasets=client_to_datasets['client_agent' + str(i)],
+                                                 evaluator=ModelEvaluator(X_test, y_test),
+                                                 active_clients=active_clients) for i in
+            range(num_clients)}  # initialize the agents
+
+        self.server_agents = {'server_agent' + str(i): ServerAgent(agent_number=i) for i in
+                              range(num_servers)}  # initialize servers
+
+        # create directory with mappings from names to instances
+        self.directory = Directory(clients=self.clients, server_agents=self.server_agents)
+
+        for agent_name, agent in self.clients.items():
+            agent.set_directory(self.directory)
+            agent.initializations()
+        for agent_name, agent in self.server_agents.items():
+            agent.set_directory(self.directory)
+
+        # OFFLINE diffie-helman key exchange
+        # NOTE: this is sequential in implementation, but simulated as occuring parallel
+        if config.USE_SECURITY:
+            key_exchange_start = datetime.datetime.now()  # measuring how long the python script takes
+            max_latencies = []
+            for client_name, client in self.clients.items():
+                # not including logic of sending/receiving public keys in latency computation since it is nearly zero
+                client.send_pubkeys()
+                max_latency = max(config.LATENCY_DICT[client_name].values())
+                max_latencies.append(max_latency)
+            simulated_time = max(max_latencies)
+
+            key_exchange_end = datetime.datetime.now()  # measuring runtime
+            key_exchange_duration = key_exchange_end - key_exchange_start
+            simulated_time += key_exchange_duration
+            if config.SIMULATE_LATENCIES:
+                print(
+                    'Diffie-helman key exchange simulated duration: {}\nDiffie-helman key exchange real run-time: {}\n'.format(
+                        simulated_time, key_exchange_duration))
+
+            for client_name, client in self.clients.items():
+                client.initialize_common_keys()
+
+    def run_simulation(self, num_iterations, server_agent_name='server_agent0'):
+        """
+        Online stage of simulation.
+        :param num_iterations: number of iterations to run
+        :param server_agent_name: which server to use. Defaults to first server.
+        """
+        # ONLINE
+        server_agent = self.directory.server_agents[server_agent_name]
+        server_agent.request_values(num_iterations=num_iterations)
+        server_agent.final_statistics()
+
---
+++ src/server_agent.py
@@ -0,0 +1,157 @@
+import sys
+sys.path.append('..')
+
+import numpy as np
+import config
+import random
+from datetime import datetime
+import multiprocessing
+from multiprocessing.pool import ThreadPool
+from utils.latency_helper import find_slowest_time
+from agent import Agent
+from message import Message
+
+def client_computation_caller(inp):
+    client_instance, message = inp
+    return_message = client_instance.produce_weights(message=message)
+    return return_message
+
+
+def client_weights_returner(inp):
+    client_instance, message = inp
+    converged = client_instance.receive_weights(message)
+    return converged
+
+
+def client_agent_dropout_caller(inp):
+    client_instance, message = inp
+    __ = client_instance.remove_active_clients(message)
+    return None
+
+
+class ServerAgent(Agent):
+    """ Server agent that averages (federated) weights and returns them to clients"""
+
+    def __init__(self, agent_number):
+        super(ServerAgent, self).__init__(agent_number=agent_number, agent_type='server_agent')
+        self.averaged_weights = {}
+        self.averaged_intercepts = {}
+
+    def request_values(self, num_iterations):
+        """
+        Method invoked to start simulation. Prints out what clients have converged on what iteration.
+        Also prints out accuracy for each client on each iteration (what weights would be if not for the simulation) and federated accuaracy.
+        :param iters: number of iterations to run
+        """
+        converged = {}  # maps client names to iteration of convergence. Contains all inactive clients
+        active_clients = set(self.directory.clients.keys())
+
+        for i in range(1, num_iterations+1):
+            weights = {}
+            intercepts = {}
+
+            m = multiprocessing.Manager()
+            lock = m.Lock()
+            with ThreadPool(len(active_clients)) as calling_pool:
+                args = []
+                for client_name in active_clients:
+                    client_instance = self.directory.clients[client_name]
+                    body = {'iteration': i, 'lock': lock, 'simulated_time':config.LATENCY_DICT[self.name][client_name]}
+                    arg = Message(sender_name=self.name, recipient_name=client_name, body=body)
+                    args.append((client_instance, arg))
+                messages = calling_pool.map(client_computation_caller, args)
+
+            server_logic_start = datetime.now()
+
+            vals = {message.sender: (message.body['weights'], message.body['intercepts']) for message in messages}
+            simulated_time = find_slowest_time(messages)
+
+            # add them to the weights_dictionary
+            for client_name, return_vals in vals.items():
+                client_weights, client_intercepts = return_vals
+                weights[client_name] = np.array(client_weights)
+                intercepts[client_name] = np.array(client_intercepts)
+
+            weights_np = list(weights.values()) # the weights for this iteration!
+            intercepts_np = list(intercepts.values())
+
+            try:
+                averaged_weights = np.average(weights_np, axis=0)  # gets rid of security offsets
+            except:
+                raise ValueError('''DATA INSUFFICIENT: Some client does not have a sample from each class so dimension of weights is incorrect. Make
+                                 train length per iteration larger for each client to avoid this issue''')
+
+            averaged_intercepts = np.average(intercepts_np, axis=0)
+            self.averaged_weights[i] = averaged_weights  ## averaged weights for this iteration!!
+            self.averaged_intercepts[i] = averaged_intercepts
+
+            # add time server logic takes
+            server_logic_end = datetime.now()
+            server_logic_time = server_logic_end - server_logic_start
+            simulated_time += server_logic_time
+
+            with ThreadPool(len(active_clients)) as returning_pool:
+                args = []
+                for client_name in active_clients:
+                    client_instance = self.directory.clients[client_name]
+                    body = {'iteration': i, 'return_weights': averaged_weights,
+                            'return_intercepts': averaged_intercepts,
+                            'simulated_time': simulated_time + config.LATENCY_DICT[self.name][client_name]}
+                    message = Message(sender_name=self.name, recipient_name=client_name, body=body)
+                    args.append((client_instance, message))
+                return_messages = returning_pool.map(client_weights_returner, args)
+
+            simulated_time = find_slowest_time(return_messages)
+            server_logic_start = datetime.now()
+            clients_to_remove = set()
+            for message in return_messages:
+                if message.body['converged'] == True and message.sender not in converged:  # converging
+                    converged[message.sender] = i  # iteration of convergence
+                    clients_to_remove.add(message.sender)
+
+            server_logic_end = datetime.now()
+            server_logic_time = server_logic_end - server_logic_start
+            simulated_time += server_logic_time
+
+            if config.CLIENT_DROPOUT:
+                # tell the clients which other clients have dropped out
+                active_clients -= clients_to_remove
+                if len(active_clients) < 2: # no point in continuing if don't have at least 2 clients
+                    self.print_convergences(converged)
+                    return
+                with ThreadPool(len(active_clients)) as calling_pool:
+                    args = []
+                    for client_name in active_clients:
+                        client_instance = self.directory.clients[client_name]
+                        body = {'clients_to_remove': clients_to_remove, 'simulated_time': simulated_time + config.LATENCY_DICT[self.name][client_name], 'iteration':i}
+                        message = Message(sender_name=self.name, recipient_name=client_name, body=body)
+                        args.append((client_instance, message))
+                    __ = calling_pool.map(client_agent_dropout_caller, args)
+
+
+        # at end of all iterations
+        self.print_convergences(converged)
+
+    def print_convergences(self, converged):
+        for client_name in self.directory.clients.keys():
+            if client_name in converged:
+                print('Client {} converged on iteration {}'.format(client_name, converged[client_name]))
+            if client_name not in converged:
+                print('Client {} never converged'.format(client_name))
+
+    def final_statistics(self):
+        # for research purposes
+        client_accs = []
+        fed_acc = []
+        for client_name, client_instance in self.directory.clients.items():
+            fed_acc.append(list(client_instance.federated_accuracy.values()))
+            client_accs.append(list(client_instance.personal_accuracy.values()))
+
+        if config.CLIENT_DROPOUT:
+            print('Federated accuracies are {}'.format(dict(zip(self.directory.clients, fed_acc))))
+        else:
+            client_accs = list(np.mean(client_accs, axis=0))
+            fed_acc = list(np.mean(fed_acc, axis=0))
+            print('Personal accuracy on final iteration is {}'.format(client_accs))
+            print('Federated accuracy on final iteration is {}'.format(fed_acc))  # should all be the same if no dropout
+
