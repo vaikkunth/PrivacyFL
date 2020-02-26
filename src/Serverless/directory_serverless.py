55d433ea081a216483897c7220b59dde29d0d131
Anton Peraire-Bueno <aperaire@mit.edu>
2020-02-26 14:47:34 -0500

    removing src

--- src/Serverless/directory_serverless.py
+++
@@ -1,6 +0,0 @@
-class Directory:
-    """
-    Contains mappings for client and server names to instances in simulation
-    """
-    def __init__(self, clients):
-        self.clients = clients
--- src/directory.py
+++
@@ -1,8 +0,0 @@
-class Directory:
-    """
-    Contains mappings for client and server names to instances in simulation
-    """
-    def __init__(self, clients, server_agents):
-        self.clients = clients
-        self.server_agents = server_agents
-        self.all_agents = {**self.clients, **self.server_agents}
