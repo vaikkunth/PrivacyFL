2739db1636bacce120d43af8274399a009a9a1e9
Anton Peraire-Bueno <aperaire@mit.edu>
2020-02-26 20:21:48 -0500

    Contains DP noise mechanisms

--- src/utils/dp_mechanisms.py
+++ src/utils/dp_mechanisms.py
@@ -2,12 +2,16 @@ from numbers import Real
 import random
 import numpy as np
 
+
+"""
+WARNING: only laplace will produce reproducible results since the other DP mechanisms 
+included here using numpy's random module which is not thread-safe for randomness. 
+"""
 def laplace(mean, sensitivity, epsilon): # mean : value to be randomized (mean)
         scale = sensitivity / epsilon
         rand = random.uniform(0,1) - 0.5 # rand : uniform random variable
         return mean - scale * np.sign(rand) * np.log(1 - 2 * np.abs(rand))
-'''
-Need to be converted from numpy to random module
+
 
 def boundedLaplace(mean, sensitivity, epsilon, delta):
     #epsion>0 and delta >0 and <0.5
@@ -72,4 +76,4 @@ def gaussian(mean, sensitivity, epsilon, delta ):
       
     return stdNormal * scale + mean
 
-'''
+
